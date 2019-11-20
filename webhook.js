fedf2a0cb0e29c01bc72c8db06b9bfc01985c8cc
cometang <1403029829@qq.com>
2019-11-20 23:27:33 +0800

    1

--- webhook.js
+++ webhook.js
@@ -1,15 +1,16 @@
 let http = require('http')
 let server = http.createServer(function (req, res) {
     //判断github发送的是不是post 是不是webhook发送的请求
+    
     if (req.method == 'POST' && req.url == '/webhook') {
         //设置github请求的请求头，设置返回数据的格式为json
         res.setHeader('Content-Type', 'application/json')
         //返回通知github请求已经成功
         res.end(JSON.stringify({ok:true}))
-        
+
 
     } else {
-        req.end('NOT Found')
+        res.end('NOT Found')
     }
 })
 
