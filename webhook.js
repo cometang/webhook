0f14b781a430df99d1c85e4bda7ac5e76bf66c26
cometang <1403029829@qq.com>
2019-11-20 23:02:41 +0800

    提交webhook

---
+++ webhook.js
@@ -0,0 +1,18 @@
+let http = require('http')
+let server = http.createServer(function (req, res) {
+    //判断github发送的是不是post 是不是webhook发送的请求
+    if (req.method == 'POST' && req.url == '/webhook') {
+        //设置github请求的请求头，设置返回数据的格式为json
+        res.setHeader('Content-Type', 'application/json')
+        //返回通知github请求已经成功
+        res.end(JSON.stringify({ok:true}))
+        
+
+    } else {
+        req.end('NOT Found')
+    }
+})
+
+server.listen(4000, () => {
+    console.log('webhook服务已经在4000端口启动')
+})
