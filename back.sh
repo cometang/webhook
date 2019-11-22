1d01322a9823c093d2eb6255df1f7a961d09a5db
cometang <1403029829@qq.com>
2019-11-23 00:04:05 +0800

    1

--- back.sh
+++ back.sh
@@ -1,6 +1,4 @@
 #!/bin/bash 
-#后端项目快速构建脚本
-#后端项目路径
 WORK_PATH='/usr/projects/back'
 cd $WORK_PATH
 echo "先清除老代码"
@@ -9,9 +7,9 @@ git clean -f
 echo "拉取新代码"
 git pull origin master
 echo "开始执行构建后端项目:back为docker镜像名称 1.0为版本号"
-docker build -t back1.0 .
+docker build -t back:1.0 .
 echo "停止旧容器 并删除旧容器"
 docker stop back-container
 docer rm back-container
 echo "启动新容器"
-docker container run -p 3000:3000 --name back-container -d back1.0
+docker container run -p 3000:3000 --name back-container -d back:1.0
